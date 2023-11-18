a9a9811eae3c80d8aeddccaeaccb152bf0678bd2
AhmedMonem122 <ahmed.monem90@yahoo.com>
2023-11-18 18:10:17 +0200

    Blood Bank Page 6

---
+++ js/main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// owl carousel init
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    // loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 5000,
+    // autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    // stagePadding: -5,
+    rtl: true,
+    margin: 20,
+    responsiveBaseElement: "body",
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
+  });
+});
+
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   dots: true,
+//   stagePadding: -5,
+//   responsiveBaseElement: "body",
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     700: {
+//       items: 2,
+//     },
+//     1220: {
+//       items: 3,
+//     },
+//   },
+// });
